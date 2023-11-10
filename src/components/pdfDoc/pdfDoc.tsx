259e71ed0fafa348d43426c540f9b3dde7238aa0
mostafa-de <mostafafayyado1@gmail.com>
2023-11-11 01:37:19 +0300

    * Add De documentation on pdf viewer.

--- src/components/pdfDoc/pdfDoc.tsx
+++ src/components/pdfDoc/pdfDoc.tsx
@@ -1,6 +1,6 @@
 import type { Component } from "solid-js";
 import PDFViewerApp from "./PDFViewerApp";
-import deDoc from "../../assets/test.pdf";
+import deDoc from "../../assets/de.pdf";
 
 const PdfDoc: Component = () => {
   const pdfFilePath = deDoc;
